476c7d32b48cccc98ff9a8511555a4d712f43526
LeeWonWoo <bacassf@gmail.com>
2019-09-20 17:08:56 +0900

    fixed untracked files

--- app/src/main/java/com/lee/mymvvmsample/network/model/ListData.kt
+++
@@ -1,3 +0,0 @@
-package com.lee.mymvvmsample.network.model
-
-data class ListData<T>(var items: List<T> = listOf(), val rowsCount: Int = 0)
--- app/src/main/java/com/lee/mymvvmsample/network/model/VersionResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.lee.mymvvmsample.network.model
-
-data class VersionResponse(
-    val platform : String,
-    val version : String?,
-    val status : Boolean
-)
