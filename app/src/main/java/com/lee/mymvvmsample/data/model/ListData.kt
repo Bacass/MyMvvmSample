96c94c878f7ba61dd15f8e7d2f2565c4c7335319
Lee Wonwoo <bacassf@gmail.com>
2025-08-09 15:52:53 +0900

    - feat : 클린아키텍처에 맞게 개선 중.

--- app/src/main/java/com/lee/mymvvmsample/data/model/ListData.kt
+++
@@ -1,3 +0,0 @@
-package com.lee.mymvvmsample.data.model
-
-data class ListData<T>(var items: List<out T> = listOf(), val rowsCount: Int = 0)
--- app/src/main/java/com/lee/mymvvmsample/data/network/NetworkResult.kt
+++
@@ -1,7 +0,0 @@
-package com.lee.mymvvmsample.data.network
-
-sealed class NetworkResult<out T : Any> {
-    class Success<out T : Any>(val response: T?) : NetworkResult<T>()
-
-    class Error(val errorCode: String?, val errorMsg: String?) : NetworkResult<Nothing>()
-}
